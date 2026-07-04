262c329b1fed3c4baae32a5d2a0d0cdc88872784
Usman-git611 <usmankur611@gmail.com>
2026-07-04 12:59:58 +0530

    js_folder(by usman)

---
+++ static/js/leave.js
@@ -0,0 +1,7 @@
+const startDate = document.querySelector("input[name='start_date']");
+const endDate = document.querySelector("input[name='end_date']");
+if (startDate && endDate) {
+  startDate.addEventListener("change", () => {
+    endDate.min = startDate.value;
+  });
+}
